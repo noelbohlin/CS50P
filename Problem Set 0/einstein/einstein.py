fc7cd3d8fc8044e34f7720f9d154b98b3d4cce14
Noel Bohlin <96952073+noelbohlin@users.noreply.github.com>
2024-01-13 11:07:57 +0100

    Update einstein.py
    shortened

--- Problem Set 0/einstein/einstein.py
+++ Problem Set 0/einstein/einstein.py
@@ -1,8 +1,7 @@
 #från uppgiften Einstein från CS50
 
 def main():
-    m = int(input("m: "))
-    E = m * (300000000 ** 2)
+    E = (int(input("m: "))) * (300000000 ** 2)
     print(f"E: {E}")
 
 main()
