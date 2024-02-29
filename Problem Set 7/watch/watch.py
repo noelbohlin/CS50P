36a79c25494918fb58f1a024ca62af0c939c8d06
Noel Bohlin <96952073+noelbohlin@users.noreply.github.com>
2024-02-29 12:40:02 +0100

    Update watch.py

--- Problem Set 7/watch/watch.py
+++ Problem Set 7/watch/watch.py
@@ -4,24 +4,10 @@ import re
 
 
 def main():
-    """
-    Main function of the program.
-
-    This function takes user input, parses it, and prints the result.
-    """
     print(parse(input("HTML: ")))
 
 
 def parse(s):
-    """
-    Parses the input string to extract the YouTube video ID.
-
-    Args:
-        s (str): The input string containing the HTML embed code.
-
-    Returns:
-        str: The URL of the YouTube video if successful, otherwise None.
-    """
     try:
         s = re.search(r"embed/(\w+)\"", s)
         return f"https://youtu.be/{s.group(1)}"
