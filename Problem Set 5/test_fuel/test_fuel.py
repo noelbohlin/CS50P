b55b93c3cc918fd421c6ceb9f2e2fe34b622ac58
Noel Bohlin <96952073+noelbohlin@users.noreply.github.com>
2024-02-29 12:34:24 +0100

    Update test_fuel.py

--- Problem Set 5/test_fuel/test_fuel.py
+++ Problem Set 5/test_fuel/test_fuel.py
@@ -5,22 +5,12 @@ import pytest
 
 
 def test_convert():
-    """
-    Tests the convert function with various inputs.
-
-    Asserts that the function correctly converts fractions to percentages.
-    """
     assert convert("1/4") == 25
     assert convert("1/1") == 100
     assert convert("0/34234") == 0
 
 
 def test_gauge():
-    """
-    Tests the gauge function with various inputs.
-
-    Asserts that the function correctly converts percentage values to string representations.
-    """    
     assert gauge(25) == "25%"
     assert gauge(0) == "E"
     assert gauge(1) == "E"
@@ -29,21 +19,11 @@ def test_gauge():
 
 
 def test_valueerror():
-    """
-    Tests the convert function with invalid inputs.
-
-    Asserts that the function raises a ValueError when given invalid inputs.
-    """
     with pytest.raises(ValueError):
         convert("5/4")
         convert("cat/dog")
 
 
 def test_zerodivisionerror():
-    """
-    Tests the convert function with division by zero.
-
-    Asserts that the function raises a ZeroDivisionError when trying to divide by zero.
-    """
     with pytest.raises(ZeroDivisionError):
         convert("4/0")
