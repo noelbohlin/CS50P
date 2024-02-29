4c018b19c3f51b69e42a1710d05284d01d915c75
Noel Bohlin <96952073+noelbohlin@users.noreply.github.com>
2024-02-29 12:40:46 +0100

    Update test_working.py

--- Problem Set 7/working/test_working.py
+++ Problem Set 7/working/test_working.py
@@ -5,9 +5,6 @@ import pytest
 
 
 def test_conversion():
-    """
-    Tests the conversion of time strings from 12-hour format to 24-hour format.
-    """
     assert convert("9 AM to 5 PM") == "09:00 to 17:00"
     assert convert("9:00 AM to 5:00 PM") == "09:00 to 17:00"
     assert convert("12:00 AM to 8:45 PM") == "00:00 to 20:45"
@@ -15,9 +12,6 @@ def test_conversion():
 
 
 def test_valueerror():
-    """
-    Tests if ValueError is raised correctly.
-    """
     with pytest.raises(ValueError):
         convert("14 AM to 3 PM")
     with pytest.raises(ValueError):
