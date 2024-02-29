286a8579631fa70ab0d82b1c3ffee21ad0d31325
Noel Bohlin <96952073+noelbohlin@users.noreply.github.com>
2024-02-29 12:42:22 +0100

    Update seasons.py

--- Problem Set 8/seasons/seasons.py
+++ Problem Set 8/seasons/seasons.py
@@ -8,13 +8,6 @@ p = inflect.engine()
 
 
 def main():
-    """
-    Main function of the program.
-
-    This function prompts the user for their date of birth, 
-    converts it to the number of days since then,
-    and prints out the result.
-    """
     try:
         year, month, day = input("Date of birth: ").split("-")
         year, month, day = int(year), int(month), int(day)
@@ -24,17 +17,6 @@ def main():
 
 
 def convert(y, m, d):
-    """
-    Converts a given date to the number of minutes since that date.
-
-    Parameters:
-    y (int): Year of the date.
-    m (int): Month of the date.
-    d (int): Day of the date.
-
-    Returns:
-    str: Number of minutes since the given date, converted to words.
-    """
     return (f"{p.number_to_words(int((date.today() - date(y, m, d)).days * 24 * 60))}"
             f" minutes").capitalize().replace("and ", "")
 
