82a7efe899a344e2715d76a5280934e6180a16fd
Noel Bohlin <96952073+noelbohlin@users.noreply.github.com>
2024-02-29 12:28:25 +0100

    Update twttr.py

--- Problem Set 2/twttr/twttr.py
+++ Problem Set 2/twttr/twttr.py
@@ -2,25 +2,10 @@
 
 
 def main():
-    """
-    Main function of the program.
-
-    This function takes an input string, converts it by removing all vowels,
-    and prints the converted string.
-    """
     print("Output:", convert(input("Input: ")))
 
 
 def convert(string):
-    """
-    Converts a given string by removing all vowels.
-
-    Args:
-        string (str): The input string.
-
-    Returns:
-        str: The converted string without vowels.
-    """
     output = ""
     for letter in string:
         output += letter.rstrip("aAeEiIoOuU")
