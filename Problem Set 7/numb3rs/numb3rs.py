537af30638792b62ed312c1b524e97655b9e609d
Noel Bohlin <96952073+noelbohlin@users.noreply.github.com>
2024-02-29 12:38:37 +0100

    Update numb3rs.py

--- Problem Set 7/numb3rs/numb3rs.py
+++ Problem Set 7/numb3rs/numb3rs.py
@@ -1,25 +1,10 @@
 """ från uppgiften Numb3rs från CS50 """
 
 def main():
-    """
-    Main function of the program.
-
-    This function prompts the user for an IP address, validates it, 
-    and prints whether it's valid or not.
-    """
     print(validate(input("IPv4 Address: ").strip()))
 
 
 def validate(ip):
-    """
-    Validates an IP address.
-
-    Args:
-        ip (str): The IP address to validate.
-
-    Returns:
-        bool: True if the IP address is valid, False otherwise.
-    """
     try:
         a, b, c, d = ip.split(".")
 
