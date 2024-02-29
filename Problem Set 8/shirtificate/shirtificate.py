4df99db556f683e933925961520dc35da925c3e2
Noel Bohlin <96952073+noelbohlin@users.noreply.github.com>
2024-02-29 12:43:00 +0100

    Update shirtificate.py

--- Problem Set 8/shirtificate/shirtificate.py
+++ Problem Set 8/shirtificate/shirtificate.py
@@ -3,15 +3,7 @@
 from fpdf import FPDF
 
 class PDF(FPDF): # pylint: disable=R0903
-    """
-    This class represents a PDF document.
-    It inherits from the FPDF class and overrides some of its methods.
-    """
     def header(self):
-        """
-        This method adds a header to the PDF document.
-        It includes an image and text.
-        """
         self.image("shirtificate.png", 10, 70, 190)
         self.set_font("helvetica", "", 55)
         self.cell(0, 57, "CS50 Shirtificate", align="C")
@@ -19,9 +11,6 @@ class PDF(FPDF): # pylint: disable=R0903
 
 
 def main():
-    """
-    Main function of the program.
-    """
     pdf = PDF()
     pdf.add_page(orientation="portrait", format="a4")
 
