3a50ef9d5d3aa41dc4e0a714b0301a01708a0553
Noel Bohlin <96952073+noelbohlin@users.noreply.github.com>
2024-02-29 12:31:45 +0100

    Update game.py

--- Problem Set 4/game/game.py
+++ Problem Set 4/game/game.py
@@ -4,10 +4,6 @@ import random
 
 
 def main():
-    """
-    Main function. It generates a random number and prompts the user to guess it.
-    The game continues until the user guesses correctly.
-    """
     n = get_random_int(get_integer("Level: "))
 
     while True:
@@ -23,28 +19,10 @@ def main():
 
 
 def get_random_int(n):
-    """
-    Generates a random integer between 1 and n.
-
-    Args:
-        n (int): The upper limit for the random number.
-
-    Returns:
-        int: A random integer between 1 and n.
-    """
     return random.randint(1, n)
 
 
 def get_integer(prompt):
-    """
-    Prompts the user for input until a valid integer greater than 0 is entered.
-
-    Args:
-        prompt (str): The string to display as a prompt to the user.
-
-    Returns:
-        int: The integer entered by the user.
-    """
     while True:
         try:
             n = int(input(prompt))
