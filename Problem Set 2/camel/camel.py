9889ad6095a3fba156adbae7f82cb2b496257e6b
Noel Bohlin <96952073+noelbohlin@users.noreply.github.com>
2024-02-29 12:27:20 +0100

    Update camel.py

--- Problem Set 2/camel/camel.py
+++ Problem Set 2/camel/camel.py
@@ -2,23 +2,10 @@
 
 
 def main():
-    """
-    Main function of the program.
-    Takes input from the user, converts it from camelCase to snake_case, and prints the result.
-    """
     print("snake_case: ", convert(input("camelCase: ")))
 
 
 def convert(string):
-    """
-    Converts a camelCase string to snake_case.
-
-    Parameters:
-    string (str): The camelCase string to convert.
-
-    Returns:
-    str: The converted snake_case string.
-    """
     output = ""
     for letter in string:
         if letter.isupper():
