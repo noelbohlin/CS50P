f8497d0cd3eeea80c8ce29fabaa19f7d21423a1e
Noel Bohlin <96952073+noelbohlin@users.noreply.github.com>
2024-02-29 12:33:15 +0100

    Update bank.py

--- Problem Set 5/test_bank/bank.py
+++ Problem Set 5/test_bank/bank.py
@@ -1,19 +1,10 @@
 """Från uppgiften test Bank från CS50 """
 
 def main():
-    """
-    Main function of the program.
-    """
     print(f"${value(input('Greeting: '))}")
 
 
 def value(greeting):
-    """
-    This function takes a greeting as input and returns a value based on the greeting.
-    If the greeting starts with "hello", it returns 0.
-    If the greeting starts with "h", it returns 20.
-    Otherwise, it returns 100.
-    """
     if greeting.lower().lstrip().startswith("hello"):
         return 0
     if greeting.lower().lstrip().startswith("h"):
