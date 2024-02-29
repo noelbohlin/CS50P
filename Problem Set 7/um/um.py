09e641f0315e548004641c26cfb1de5c52b40f77
Noel Bohlin <96952073+noelbohlin@users.noreply.github.com>
2024-02-29 12:39:20 +0100

    Update um.py

--- Problem Set 7/um/um.py
+++ Problem Set 7/um/um.py
@@ -4,23 +4,10 @@ import re
 
 
 def main():
-    """
-    Main function of the program.
-    Prompts the user for input and prints the count of 'um' in the input string.
-    """
     print(count(input("Text: ")))
 
 
 def count(s):
-    """
-    Counts the number of occurrences of the word 'um' in the input string.
-
-    Parameters:
-    s (str): The input string.
-
-    Returns:
-    int: The number of occurrences of 'um'.
-    """
     return len(re.findall(r"(\W|^)um(\W|$)", s, re.IGNORECASE))
 
 
