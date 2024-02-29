446a5765a0b0e59595f1ca37ad68ab89630b77bd
Noel Bohlin <96952073+noelbohlin@users.noreply.github.com>
2024-02-29 12:31:22 +0100

    Update figlet.py

--- Problem Set 4/figlet/figlet.py
+++ Problem Set 4/figlet/figlet.py
@@ -8,28 +8,17 @@ figlet = Figlet()
 
 
 def main():
-    """
-    Main function of the program. Detects invalid input, sets the font, and prints the output.
-    """
     detect_invalid_input()
     set_font()
     print("Output:\n", figlet.renderText(input("Input: ")))
 
 
 def detect_invalid_input():
-    """
-    Checks if the number of command line arguments is valid.
-    Exits the program if the usage is invalid.
-    """
     if len(sys.argv) != 1 and len(sys.argv) != 3:
         sys.exit("Invalid Usage")
 
 
 def set_font():
-    """
-    Sets the font for the figlet object. If no arguments are passed, a random font is selected.
-    If a font argument is passed, it sets the font to the specified one if it exists.
-    """
     if len(sys.argv) == 1:
         figlet.setFont(
             font=(figlet.getFonts()[random.randint(0, len(figlet.getFonts()))])
