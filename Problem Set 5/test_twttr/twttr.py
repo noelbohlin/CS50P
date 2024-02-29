5a714ba1a7b7a943949f93fa3679ee7884f0e2bd
Noel Bohlin <96952073+noelbohlin@users.noreply.github.com>
2024-02-29 12:36:09 +0100

    Update twttr.py

--- Problem Set 5/test_twttr/twttr.py
+++ Problem Set 5/test_twttr/twttr.py
@@ -1,23 +1,10 @@
 """ Från uppgiften Test Twitter från CS50 """
 
 def main():
-    """
-    Main function of the program. Takes an input string, shortens it by removing all vowels, 
-    and prints the result.
-    """
     print("Output:", shorten(input("Input: ")))
 
 
 def shorten(word):
-    """
-    Shortens the given word by removing all vowels.
-
-    Parameters:
-    word (str): The word to be shortened.
-
-    Returns:
-    str: The shortened word.
-    """
     output = ""
     for letter in word:
         output += letter.rstrip("aAeEiIoOuU")
