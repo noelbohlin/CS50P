365141b3f1419cf310f6b38fd3d0fb938798d563
Noel Bohlin <96952073+noelbohlin@users.noreply.github.com>
2024-02-29 12:29:14 +0100

    Update fuel.py

--- Problem Set 3/fuel/fuel.py
+++ Problem Set 3/fuel/fuel.py
@@ -2,12 +2,6 @@
 
 
 def main():
-    """
-    Main function of the program.
-
-    This function gets the fuel state as a fraction from the user,
-    checks the fuel state, and prints out the corresponding character or percentage.
-    """
     fuelstate = get_fraction("Fraction: ")
 
     if fuelstate <= 0.01:
@@ -18,17 +12,7 @@ def main():
         print(f"{round(fuelstate * 100)}%")
 
 
-# gets decimal number from input
 def get_fraction(prompt):
-    """
-    Prompts the user for a fraction and returns it as a decimal.
-
-    Args:
-        prompt (str): The string to display as a prompt to the user.
-
-    Returns:
-        float: The decimal equivalent of the fraction entered by the user.
-    """
     while True:
         try:
             x, y = input(prompt).split("/")
